2d13f929dd6f50165b51457fd1c06e5d89c913e0
benzson92 <benzson.v@gmail.com>
2020-11-23 00:04:03 +0700

    store for form and list
    local storage function,
    personal info data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
-export default configureStore({
+import infoPersonalReducer from '../features/infoPersonal/infoPersonalSlice'
+
+// convert object to string and store in localStorage
+export function saveToLocalStorage(state) {
+  console.log(state);
+  if (typeof window !== 'undefined') {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem('state', serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+}
+
+// load string from localStarage and convert back in to an Object
+// invalid output must be undefined
+function loadFromLocalStorage() {
+  if (typeof window !== 'undefined') {
+    try {
+      const serialisedState = localStorage.getItem('state');
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+}
+const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    infoPersonal: infoPersonalReducer
   },
+  preloadedState: loadFromLocalStorage()
 });
+
+export default store
